9cefd46e371cee1a3c07378d877d8c1aa1b8b77a
pes751091-creator <pes751091@gmail.com>
2026-01-02 12:21:02 +0530

    Set DATABASE_URI2 to MongoDB connection string
    Updated DATABASE_URI2 to a specific MongoDB connection string.

--- info.py
+++ info.py
@@ -79,7 +79,7 @@ COLLECTION_NAME = environ.get('COLLECTION_NAME', 'dreamcinezone_files') # Collec
 
 # If MULTIPLE_DB Is True Then Fill DATABASE_URI2 Value Else You Will Get Error.
 MULTIPLE_DB = is_enabled(os.environ.get('MULTIPLE_DB', "False"), False) # Type True For Turn On MULTIPLE DB FUNTION 
-DATABASE_URI2 = environ.get('DATABASE_URI2', "")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
+DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://deepikapadukone1234566_db_user:Adarsh12345@cluster0.7htiegs.mongodb.net/?appName=Cluster0")  # MongoDB URI for the second database (if MULTIPLE_DB is True)
 # ============================
 # Movie Notification & Update Settings
 # ============================
